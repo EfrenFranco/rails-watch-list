fac0976fe0d9ee66736c78e85c06db2a9e17441a
Efren Franco <efrenrfranco@gmail.com>
2023-08-11 13:21:19 +0900

    added stuff

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,9 @@
 class List < ApplicationRecord
+
+  has_one_attached :photo
   validates :name, uniqueness: true
   validates :name, presence: true
   has_many :bookmarks
   has_many :movies, through: :bookmarks, dependent: :destroy
+
 end
